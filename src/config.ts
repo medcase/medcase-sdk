8e7d14ad372483e711421962e92d7c876d6ed7d2
Marek Kedzia <marek.kedzia1@o2.pl>
2023-09-01 16:02:11 +0200

    remove .env file

--- src/config.ts
+++ src/config.ts
@@ -1,16 +1,8 @@
-import * as process from "process";
-
-export const required = (key: string, variable?: string) => {
-    if (!variable)
-        throw new Error(`Required property is missing: ${key} on level ${process.env.NODE_ENV}`);
-    return variable;
-};
-
 export const appConfig = {
-    MEDCASE_STAGING_API_URL: required("MEDCASE_STAGING_API_URL", process.env.MEDCASE_STAGING_API_URL),
-    MEDCASE_PRODUCTION_API_URL: required("MEDCASE_PRODUCTION_API_URL", process.env.MEDCASE_PRODUCTION_API_URL),
-    MEDCASE_OAUTH_URL_STAGING: required("MEDCASE_OAUTH_URL_STAGING", process.env.MEDCASE_OAUTH_URL_STAGING),
-    MEDCASE_OAUTH_AUDIENCE_STAGING: required("MEDCASE_OAUTH_AUDIENCE_STAGING", process.env.MEDCASE_OAUTH_AUDIENCE_STAGING),
-    MEDCASE_OAUTH_URL_PRODUCTION: required("MEDCASE_OAUTH_URL_PRODUCTION", process.env.MEDCASE_OAUTH_URL_PRODUCTION),
-    MEDCASE_OAUTH_AUDIENCE_PRODUCTION: required("MEDCASE_OAUTH_AUDIENCE_PRODUCTION", process.env.MEDCASE_OAUTH_AUDIENCE_PRODUCTION),
+    MEDCASE_STAGING_API_URL: "https://api.staging.medcase.app",
+    MEDCASE_PRODUCTION_API_URL: "https://api.prod.medcase.app",
+    MEDCASE_OAUTH_URL_STAGING: "https://staging-medcase.eu.auth0.com/oauth/token",
+    MEDCASE_OAUTH_AUDIENCE_STAGING: "https://staging-medcase.marketplace.api",
+    MEDCASE_OAUTH_URL_PRODUCTION: "https://production-medcase.eu.auth0.com/oauth/token",
+    MEDCASE_OAUTH_AUDIENCE_PRODUCTION: "https://production-medcase.marketplace.api",
 };
