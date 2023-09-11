7b0b55c97f4cb904249a141a11baf6589d4a5c16
Marek Kędzia <96777780+marekkedzia@users.noreply.github.com>
2023-09-11 14:37:19 +0200

    add telehealth routes

--- src/api/clients/availabilities.client.ts
+++ src/api/clients/availabilities.client.ts
@@ -1,4 +1,4 @@
-import {Availability} from "../schemas/availability";
+import {Availability} from "../schemas";
 import {ApiClient} from "./api.client";
 import {AxiosResponse} from "axios";
 import {HttpMethod} from "../schemas/http.method";
